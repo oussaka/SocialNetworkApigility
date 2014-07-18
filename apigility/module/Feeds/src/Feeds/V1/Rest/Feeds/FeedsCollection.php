96ec8c21cc1c1541fe480fa31858daf8b2d93134
oussamak <oussaamaa@gmail.com>
2014-07-18 11:30:14 +0100

    remove old apigility directory

--- apigility/module/Feeds/src/Feeds/V1/Rest/Feeds/FeedsCollection.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Feeds\V1\Rest\Feeds;
-
-use Zend\Paginator\Paginator;
-
-class FeedsCollection extends Paginator
-{
-}
--- apigility/module/Feeds/src/Feeds/V1/Rest/Feeds/FeedsEntity.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace Feeds\V1\Rest\Feeds;
-
-class FeedsEntity
-{
-}
